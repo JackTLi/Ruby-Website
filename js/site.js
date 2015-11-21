4d03715b3ff2c3eabdae73838914337107df4938
Jack Teng Li <jt3li@uwaterloo.ca>
2015-11-21 00:51:42 -0500

    Add animation to About scroll

--- js/site.js
+++ js/site.js
@@ -1,12 +1,20 @@
 $(window).load(function(){
 	makeImageBoxSquare();
- })
+ });
 
 $(window).resize(function() {
 	makeImageBoxSquare();
-})
+});
 
 function makeImageBoxSquare() {
 	var w = $('.imageBox').width();
 	$('.topHalf').css('height', w);
 }
+
+$(document).ready(function() {
+
+	$(".sliding-link").click(function(event) {
+	    event.preventDefault();
+	    $('html,body').animate( { scrollTop:$(this.hash).offset().top } , 700);
+    } );
+});
