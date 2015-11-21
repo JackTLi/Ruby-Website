5af2150f06985725a39c6c329aeb48ebb6b48889
Jack Teng Li <jt3li@uwaterloo.ca>
2015-11-21 02:38:40 -0500

    Finished slide out nav

--- js/site.js
+++ js/site.js
@@ -1,15 +1,3 @@
-$(window).load(function(){
-	makeImageBoxSquare();
- });
-
-$(window).resize(function() {
-	makeImageBoxSquare();
-});
-
-function makeImageBoxSquare() {
-	var w = $('.imageBox').width();
-	$('.topHalf').css('height', w);
-}
 
 $(document).ready(function() {
 
