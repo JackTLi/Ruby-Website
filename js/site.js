3dcda058dc218ee52f613ddf9bafea96f24aab74
Jack Teng Li <jt3li@uwaterloo.ca>
2015-11-21 02:28:50 -0500

    Finished nav on home screen for slide out portfolio nav

--- js/site.js
+++ js/site.js
@@ -13,8 +13,37 @@ function makeImageBoxSquare() {
 
 $(document).ready(function() {
 
+	var navSliderWidth = $('.navSlider').width();
+	$('.navSlider').css("right", (-1 * navSliderWidth)+ 'px');
+
 	$(".sliding-link").click(function(event) {
 	    event.preventDefault();
 	    $('html,body').animate( { scrollTop:$(this.hash).offset().top } , 700);
     } );
+
+    document.querySelector( "#nav-toggle" )
+      .addEventListener( "click", function() {
+        this.classList.toggle( "active" );
+        if (this.classList.contains('active'))
+        {
+        	openNavSlider();
+        }
+        else {
+        	closeNavSlider();
+        }
+      });
 });
+
+function openNavSlider () {
+    var navSliderWidth = $('.navSlider').width();
+
+	$('.navSlider').animate( {"right": 0 }, 300);
+	$('.nav .links ul').animate( {"margin-right": navSliderWidth - 50}, 300);
+}
+
+function closeNavSlider () {
+    var navSliderWidth = $('.navSlider').width();
+
+	$('.navSlider').animate( {"right": (-1 * navSliderWidth) }, 300);
+	$('.nav .links ul').animate( {"margin-right" : 0}, 300);
+}
